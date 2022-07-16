b85a8f27e98bc1f7a691f71bc9d41e099776adbb
Ayush Gupta <ayushgupta01195@gmail.com>
2022-07-16 16:06:42 +0530

    Newline character in install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if ! command -v git &> /dev/null; then
     BASE_DIR=~/Downloads/pyviva.zip
-    echo "git is not already installed, instead downloading the project zip at $BASE_DIR"
+    echo -e "\n git is not already installed, instead downloading the project zip at $BASE_DIR \n"
     wget https://github.com/GuptaAyush19/pyviva/archive/master.zip -O $BASE_DIR
     python -m pip install $BASE_DIR
 else
