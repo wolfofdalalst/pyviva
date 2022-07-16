4278081bd85d54df108671e157133aee9bdc02f3
Ayush Gupta <72538480+GuptaAyush19@users.noreply.github.com>
2022-07-16 17:57:32 +0530

    Update install.ps1

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,11 +1,5 @@
-# credit to https://stackoverflow.com/users/5640342/chrisb
-# https://stackoverflow.com/questions/23070299/get-the-windows-download-folders-path
-function GetDownloadsPath() {
-    Environ$("USERPROFILE") + "\Downloads"
-}
-
 $SOURCE = "https://github.com/GuptaAyush19/pyviva/archive/master.zip"
-$DESTINATION= GetDownloadsPath + "\pyviva.zip"
+$DESTINATION=  "$HOME\Downloads\pyviva.zip"
 
 Invoke-RestMethod -Uri $source -OutFile $destination
 
