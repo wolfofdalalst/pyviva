6092f45da42b864c7dd0bc584687756ee0d18093
Ayush Gupta <72538480+GuptaAyush19@users.noreply.github.com>
2022-07-16 17:49:07 +0530

    Update install.ps1

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,11 +1,11 @@
 # credit to https://stackoverflow.com/users/5640342/chrisb
 # https://stackoverflow.com/questions/23070299/get-the-windows-download-folders-path
-Function GetDownloadsPath() As String
-    GetDownloadsPath = Environ$("USERPROFILE") & "\Downloads"
-End Function
+function GetDownloadsPath() {
+    GetDownloadsPath = Environ$("USERPROFILE") + "\Downloads"
+}
 
 $SOURCE = "https://github.com/GuptaAyush19/pyviva/archive/master.zip"
-$DESTINATION= GetDownloadsPath() & "\pyviva.zip"
+$DESTINATION= GetDownloadsPath() + "\pyviva.zip"
 
 Invoke-RestMethod -Uri $source -OutFile $destination
 
@@ -13,5 +13,5 @@ if (Get-Command 'python') {
     # TODO: check for correct version of python
     python -m pip install $DESTINATION
 } else {
-    Write-Host "this project requires python3.7+ to run, you can download it from https://www.python.org/"
+    Write-Host "this project requires python3.8+ to run, you can download it from https://www.python.org/"
 }
